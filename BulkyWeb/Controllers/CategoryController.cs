5edf0a128ae49ce074cf7226aab2222714040d79
Ivan Rusev <ivanrusev03@schoolmath.eu>
2024-06-21 00:40:16 +0300

    feat: add Cateogry Edit and Delete functionalities

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -32,8 +32,65 @@ namespace BulkyWeb.Controllers
 
             _context.Categories.Add(category);
             _context.SaveChanges();
-            
+
+            TempData["success"] = "Category created successfully!";
+
             return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id is null)
+                return NotFound();
+
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category is null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+
+				TempData["success"] = "Category edited successfully!";
+
+				return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+		public IActionResult Delete(int? id)
+		{
+			if (id is null)
+				return NotFound();
+
+			var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+			if (category is null)
+				return NotFound();
+
+			return View(category);
 		}
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category is null)
+                return NotFound();
+
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+
+			TempData["success"] = "Category deleted successfully!";
+
+			return RedirectToAction("Index");
+        }
+					
 	}
 }
