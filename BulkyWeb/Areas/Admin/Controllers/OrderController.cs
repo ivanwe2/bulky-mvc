24d12262ed56b52e6a42e0d65b82f19209805690
Ivan Rusev <Ivan.Rusev@indeavr.com>
2024-06-25 17:56:32 +0300

    fix: add proper order management view with datatables

---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Bulky.DataAccess.Repositories.Abstractions;
+using Bulky.Models.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> products = _unitOfWork.OrderHeader.GetAllBy(null, "ApplicationUser").ToList();
+
+            return Json(new { data = products });
+        }
+        
+        #endregion
+    }
+}
